e3759c565ecfaec86a4650eb7d0c81ab3f35e096
Maryam Dar <maryamdar1384@gmail.com>
2025-08-30 22:23:23 +0330

    add no lint

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,4 @@
 "use client"
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
 import {Button} from "@/components/ui/button"
 import {
   CommandDialog,
