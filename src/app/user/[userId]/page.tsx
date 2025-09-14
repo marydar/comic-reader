04b1aa6aa70c5fd1766cfd3f6a92ab20690e901c
Maryam Dar <maryamdar1384@gmail.com>
2025-09-14 13:25:48 +0330

    fix build

--- src/app/user/[userId]/page.tsx
+++ src/app/user/[userId]/page.tsx
@@ -1,11 +1,10 @@
 "use client"
-import  UserPageNavbar  from './navbar'
 import React from 'react'
 import { useUserId } from '@/hooks/use-user-id';
 import { useRouter } from 'next/navigation';
 import { Loader } from 'lucide-react';
 
-export default function UserPage({children}:{children:React.ReactNode}) {
+export default function UserPage() {
     const userId = useUserId();
     const router = useRouter();
     if(!userId) return <div>no user id</div>
